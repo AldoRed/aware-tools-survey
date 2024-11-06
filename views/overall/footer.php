9e2af0a860ffddd29e5b11553c9b39bbb78ead5b
AldoRed <aldo@aldored.com>
2024-11-06 14:25:43 -0300

    Update footer link to point to GitHub profile

--- views/overall/footer.php
+++ views/overall/footer.php
@@ -1,6 +1,6 @@
 <div class="footer" style="color:black; background:white; text-align:center;">
     <div style="padding:3px;" class="container">
-        <a href="https://aldored.com/portfolio/en" target="_blank">
+        <a href="https://github.com/AldoRed" target="_blank">
             <p style="padding: 0;font-size:15px;margin: 0;font-weight: 100;">2024 Sitio desarrollado por AldoRed</p>
         </a>
     </div>
