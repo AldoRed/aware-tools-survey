2429a75fb79c4f833db5497f0438b02767c6eec8
AldoRed <aldo@aldored.com>
2024-11-11 20:04:14 -0300

    Fix url bug with more parameters

--- views/overall/encuesta.php
+++ views/overall/encuesta.php
@@ -134,7 +134,7 @@ $formulario = $formulario[$encuesta["id"]];
         left:0;
         width: 100%;
         height: 100%;
-        background-image: linear-gradient(180deg, #0000008c 0%, #0000008c 100%), url(<?php echo $encuesta["imagen"] ?>);
+        background-image: linear-gradient(180deg, #0000008c 0%, #0000008c 100%), url(<?php echo $url . $encuesta["imagen"] ?>);
         background-size:cover;
         clip-path: polygon(0 0, 100% 0, 100% 80%, 50% 95%, 0 80%);
         z-index:-1;
