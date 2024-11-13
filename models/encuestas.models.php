18644d35ee1e421ace8133b7b71c79d5703373c7
AldoRed <aldo@aldored.com>
2024-11-13 02:22:20 -0300

    Add functionality for creating surveys and checking slug uniqueness

--- models/encuestas.models.php
+++ models/encuestas.models.php
@@ -32,6 +32,48 @@ class ModelEncuestas{
 
     }
 
+    static public function mdlCrearEncuesta($tabla, $datos){
+
+        $stmt = Conexion::conectar()->prepare("INSERT INTO $tabla(token, nombre, slug, descripcion, imagen, secciones, creador) VALUES (:token, :nombre, :slug, :descripcion, :imagen, :secciones, :correoCreador)");
+
+        $stmt -> bindParam(":token", $datos["token"], PDO::PARAM_STR);
+        $stmt -> bindParam(":nombre", $datos["nombre"], PDO::PARAM_STR);
+        $stmt -> bindParam(":slug", $datos["slug"], PDO::PARAM_STR);
+        $stmt -> bindParam(":descripcion", $datos["descripcion"], PDO::PARAM_STR);
+        $stmt -> bindParam(":imagen", $datos["imagen"], PDO::PARAM_STR);
+        $stmt -> bindParam(":secciones", $datos["secciones"], PDO::PARAM_STR);
+        $stmt -> bindParam(":correoCreador", $datos["correoCreador"], PDO::PARAM_STR);
+
+        if($stmt -> execute()){
+
+            return "ok";
+
+        }else{
+
+            return "error";
+
+        }
+
+        $stmt -> close();
+
+        $stmt = null;
+    }
+
+    static public function mdlCheckearSlug($tabla, $slug){
+
+        $stmt = Conexion::conectar()->prepare("SELECT slug FROM $tabla WHERE slug = :slug");
+
+        $stmt->bindParam(":slug", $slug, PDO::PARAM_STR);
+
+        $stmt->execute();
+
+        $result = $stmt->fetch();
+
+        $stmt->closeCursor();
+
+        return $result ? 1 : 0;
+    }
+
     static public function mdlEditarAdminEncuesta($tabla, $datos){
 
         $stmt = Conexion::conectar()->prepare("UPDATE $tabla SET administradores = :admins WHERE id = :id");
--- views/overall/admin/crearEncuesta.php
+++ views/overall/admin/crearEncuesta.php
@@ -1,114 +1,110 @@
 <section id="crearEncuesta">
     <div class="container">
         <h2>Crear encuesta</h2>
-        <form method="post" enctype="multipart/form-data">
-            <div class="form-group">
-                <label for="nombreEncuesta">Nombre de la encuesta</label>
-                <input type="text" class="form-control" name="nombreEncuesta" placeholder="Ejemplo: Encuesta de ciberseguridad" required>
-                <span class="text-danger" id="slugError"></span>
-            </div>
+        <div class="form-group">
+            <label for="nombreEncuesta">Nombre de la encuesta</label>
+            <input type="text" class="form-control" name="nombreEncuesta" placeholder="Ejemplo: Encuesta de ciberseguridad" required>
+            <span class="text-danger" id="slugError"></span>
+        </div>
 
-            <div class="form-group">
-                <label for="slugEncuesta">Slug de la encuesta</label>
-                <input type="text" class="form-control" name="slugEncuesta" placeholder="Se generará automáticamente al modificar nombre de encuesta" readonly>
-            </div>
+        <div class="form-group">
+            <label for="slugEncuesta">Slug de la encuesta</label>
+            <input type="text" class="form-control" name="slugEncuesta" placeholder="Se generará automáticamente al modificar nombre de encuesta" readonly>
+        </div>
 
-            <div class="form-group">
-                <label for="descripcionEncuesta">Descripción de la encuesta</label>
-                <textarea class="form-control" name="descripcionEncuesta" rows="3" placeholder="Ejemplo: Encuesta para medir la ciberseguridad en las empresas" required></textarea>
-            </div>
+        <div class="form-group">
+            <label for="descripcionEncuesta">Descripción de la encuesta</label>
+            <textarea class="form-control" name="descripcionEncuesta" rows="3" placeholder="Ejemplo: Encuesta para medir la ciberseguridad en las empresas" required></textarea>
+        </div>
 
-            <div class="form-group">
-                <label for="imagenEncuesta">Imagen de la encuesta</label>
-                <small>Se recomienda una con mismo ratio entre encuestas que se quieren realizar</small>
-                <input type="file" class="form-control" name="imagenEncuesta">
-            </div>
+        <div class="form-group">
+            <img id="imagenEncuestaPreview" src="" alt="Imagen de la encuesta" style="max-width: 250px; margin-top: 5px; display:none;">
+            <label for="imagenEncuesta">Imagen de la encuesta</label>
+            <small>Se recomienda una con mismo ratio entre encuestas que se quieren realizar en conjunto</small>
+            <input type="file" id="imagenEncuesta" class="form-control" name="imagenEncuesta">
+        </div>
 
-            <div class="form-group">
-                <!-- Crear secciones de la encuesta -->
-                <table id="secciones" class="table-responsive table">
-                <button type="button" class="btn btn-primary" id="crearSeccion">Nueva sección</button>
-                    <thead>
-                        <tr>
-                            <td>Sección</td>
-                            <td>Preguntas</td>
-                        </tr>
-                    </thead>
+        <div class="form-group">
+            <!-- Crear secciones de la encuesta -->
+            <table id="secciones" class="table-responsive table">
+            <button type="button" class="btn btn-primary" id="btnCrearSeccion">Nueva sección</button>
+                <thead>
+                    <tr>
+                        <td>Sección</td>
+                        <td>Preguntas</td>
+                    </tr>
+                </thead>
 
-                    <!-- Ejemplo para maquetar secciones -->
-                    <tbody>
-                        <tr>
-                            <td>
-                                <input type="text" class="form-control" name="seccion[]" placeholder="Ejemplo: Información básica" required>
-                            </td>
-                            <td>
-                                <button type="button" class="btn btn-success" onclick="crearPregunta(this)">Nueva pregunta</button>
-                                <table class="table table-responsive" id="preguntasSeccion1">
-                                    <thead>
-                                        <tr>
-                                            <td>Pregunta</td>
-                                            <td>Tipo</td>
-                                            <td>Alternativas</td>
-                                            <td>Obligatorio</td>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                <input type="text" class="form-control" name="pregunta[0][]" placeholder="Ejemplo: ¿Cuál es su experiencia en ciberseguridad? *" required>
-                                            </td>
-                                            <td>
-                                                <select class="form-control" name="tipo[0][]">
-                                                    <option value="radio">Selección única</option>
-                                                    <option value="radioMultiple">Selección múltiple</option>
-                                                    <option value="text">Texto</option>
-                                                </select>
-                                            </td>
-                                            <td>
-                                                <input type="text" class="form-control" name="alternativa[0][]" placeholder="Ejemplo: Menos de 2 años">
-                                                <input type="text" class="form-control" name="alternativa[0][]" placeholder="Ejemplo: Entre 2 y 5 años">
-                                                <input type="text" class="form-control" name="alternativa[0][]" placeholder="Ejemplo: Más de 5 años">
-                                                <i class="fas fa-plus agregarAlternativaInput"></i>
-                                            </td>
-                                            <td>
-                                                <input type="checkbox" value="true">
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <input type="text" class="form-control" name="pregunta[0][]" placeholder="Ejemplo: Mencione brevemente su área de especialización">
-                                            </td>
-                                            <td>
-                                                <select class="form-control" name="tipo[0][]">
-                                                    <option value="radio">Selección única</option>
-                                                    <option value="radioMultiple">Selección múltiple</option>
-                                                    <option value="text">Texto</option>
-                                                </select>
-                                            </td>
-                                            <td>
-                                                <input type="text" class="form-control">
-                                                <i class="fas fa-plus agregarAlternativaInput"></i>
-                                            </td>
-                                            <td>
-                                                <input type="checkbox" name="obligatorio[0][]" value="true">
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+                <!-- Ejemplo para maquetar secciones -->
+                <tbody>
+                    <tr>
+                        <td>
+                            <input type="text" class="form-control" placeholder="Ejemplo: Información básica" value="SECCIÓN A: Preguntas relativas a la investigación realizada">
+                        </td>
+                        <td>
+                            <button type="button" class="btn btn-success agregarNuevaPregunta">Nueva pregunta</button>
+                            <table class="table table-responsive">
+                                <thead>
+                                    <tr>
+                                        <td>Pregunta</td>
+                                        <td>Tipo</td>
+                                        <td>Alternativas</td>
+                                        <td>Obligatoria</td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <input type="text" class="form-control nombrePregunta" placeholder="Ejemplo: ¿Cuál es su experiencia en ciberseguridad? *" value="¿Qué días trabaja? *">
+                                        </td>
+                                        <td>
+                                            <select class="form-control tipoPregunta">
+                                                <option value="radio">Selección única</option>
+                                                <option value="radioMultiple" selected>Selección múltiple</option>
+                                                <option value="text">Texto</option>
+                                            </select>
+                                        </td>
+                                        <td>
+                                            <input type="text" class="form-control" placeholder="Ejemplo: Menos de 2 años" value="Lunes">
+                                            <input type="text" class="form-control" placeholder="Ejemplo: Entre 2 y 5 años" value="Martes">
+                                            <input type="text" class="form-control" placeholder="Ejemplo: Más de 5 años" value="Miercoles">
+                                            <button type="button" class="btn btn-success agregarNuevaAlternativa"><i class="fas fa-plus"></i></button>
+                                        </td>
+                                        <td>
+                                            <input class="form-control" type="checkbox" checked>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <input type="text" class="form-control nombrePregunta" placeholder="Ejemplo: Mencione brevemente su área de especialización" value="Acontecimiento icónico">
+                                        </td>
+                                        <td>
+                                            <select class="form-control tipoPregunta">
+                                                <option value="radio">Selección única</option>
+                                                <option value="radioMultiple">Selección múltiple</option>
+                                                <option value="text" selected>Texto</option>
+                                            </select>
+                                        </td>
+                                        <td></td>
+                                        <td>
+                                            <input class="form-control" type="checkbox" checked>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
 
-            <!-- Correo del creador de la encuesta -->
-            <div class="form-group">
-                <label for="correoCreador">Correo del creador de la encuesta</label>
-                <input type="email" class="form-control" name="correoCreador" placeholder="Tu correo con el que administrarás la encuesta" required>
-            </div>
+        <!-- Correo del creador de la encuesta -->
+        <div class="form-group">
+            <label for="correoCreador">Correo del creador de la encuesta</label>
+            <input type="email" class="form-control" name="correoCreador" placeholder="Tu correo con el que administrarás la encuesta">
+        </div>
 
-            <button type="submit" class="btn col-xs-12">Enviar</button>
-        </form>
+        <button id="enviarCrearEncuesta" class="btn col-xs-12">Enviar</button>
     </div>
 </secion>
 
