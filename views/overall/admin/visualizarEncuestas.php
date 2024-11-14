990125c80e6b37c9ef24a0d66b458a3c9d4fac66
AldoRed <aldo@aldored.com>
2024-11-13 22:53:45 -0300

    Refactor survey display to use list group format and improve layout with responsive design

--- views/overall/admin/visualizarEncuestas.php
+++ views/overall/admin/visualizarEncuestas.php
@@ -1,33 +1,49 @@
 <div class="container">
 <h2 class="">Las encuestas aprobadas son:</h2>
-<div class="row">
+<ul class="list-group">
 <?php
 foreach ($encuestas as $key => $value) {
 
     // Create cards with the name of the survey and a link to it
     echo '
-    <div class="card col-xs-12 col-sm-6 col-md-4">
-      <img src="'.$url.$value["imagen"].'" class="card-img-top" alt="'.$value["nombre"].'">
-      <div class="card-body">
-        <h5 class="card-title">'.$value["nombre"].'</h5>
-
-        <div class="row">
-            <div class="col-xs-6">
-                <center>
-                <a href="'.$url.$value["slug"].'" class="btn btn-default">Ver</a>
-                </center>
+        <li class="list-group-item">
+            <div class="row">
+                <div class="col-sm-2">
+                    <img src="'.$url.$value["imagen"].'" class="img-responsive img-thumbnail" alt="'.$value["nombre"].'">
+                </div>
+                <div class="col-sm-8">
+                    <h4>'.$value["nombre"].'</h4>
+                    <p>'.$value["descripcion"].'</p>
+                </div>
+                <div class="col-sm-2 text-right">
+                    <a href="'.$url.$value["slug"].'" class="btn btn-primary btn-sm">Ver</a>
+                    <a href="'.$url.'admin/editar-encuesta/'.$value["slug"].'" class="btn btn-secondary btn-sm">Editar</a>
+                </div>
             </div>
-            <div class="col-xs-6">
-                <center>
-                <a href="'.$url.'admin/editar-encuesta/'.$value["slug"].'" class="btn btn-default">Editar</a>
-                </center>
-            </div>
-        </div>
-      </div>
-    </div>';
+        </li>';
+    
 }
 
 ?>
-</div>
+</ul>
 
 </div>
+
+<style>
+.list-group-item {
+    margin-bottom: 10px;
+    padding: 15px;
+    border-radius: 5px;
+    border: 1px solid #ddd;
+}
+
+.list-group-item img {
+    max-height: 80px;
+}
+
+.list-group-item h4 {
+    margin-top: 0;
+    margin-bottom: 5px;
+    font-weight: bold;
+}
+</style>
